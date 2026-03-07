d5f2f4acc63a12dbf4e48054c155efbc3d79bc74
Syming2002 <simon.bruneau@yahoo.com>
2026-03-07 15:36:50 +0100

    Updating the background page and the main page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 // alert("Coucou");
 
 const menuBurger = document.getElementById("menu-burger-toggle");
-const redStroke = document.querySelector(".dark-blue-rectangle");
+const darkBlueRectangle = document.querySelector(".dark-blue-rectangle");
 const navBar = document.getElementById("nav-bar");
 
 function openMenuMobile() {
-    if (!menuBurger || !redStroke || !navBar) return;
+    if (!menuBurger || !darkBlueRectangle || !navBar) return;
     if (menuBurger.checked) {
-        redStroke.classList.add("active");
+        darkBlueRectangle.classList.add("active");
     } else {
-        redStroke.classList.remove("active");
+        darkBlueRectangle.classList.remove("active");
     }
 }
 
-openMenuMobile();
 menuBurger.addEventListener("change", openMenuMobile);
+
+function onLogoClick() {
+    window.location.href = "/index.html";
+} 
